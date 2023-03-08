a330e32efa543b076550b0a2a46ad3341cc7f859
FRANCISCO EDUARDO VARGAS R <124694081+Fe-VraL2z@users.noreply.github.com>
2023-03-08 15:17:14 -0600

    Add files via upload
    trabajos

---
+++ Guitar.java
@@ -0,0 +1,9 @@
+public class Guitar {
+     private String serialnumber;
+    private double price;
+    private String builder;
+    private String model;
+    private String type;
+    private String backgroun;
+    private String topnoode;
+}
