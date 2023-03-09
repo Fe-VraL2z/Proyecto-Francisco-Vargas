cefcddd5a228384e4a4d5dc02bc36162c40d0728
FRANCISCO EDUARDO VARGAS R <124694081+Fe-VraL2z@users.noreply.github.com>
2023-03-09 12:43:45 -0600

    Add files via upload

---
+++ nutriologo/src/datosNutriologo.java
@@ -0,0 +1,50 @@
+public class datosNutriologo {
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    private String nombre;
+    private int edad;
+    private String sexo;
+    private double estatura;
+    private double peso;
+
+
+
+}
